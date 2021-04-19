ba75d09eb2ab3a22fd84312baf642b8e7353be34
Pavlomat <75501963+Pavlomat@users.noreply.github.com>
2021-04-19 11:59:08 +0300

    Add files via upload

---
+++ PhotoLibrary/PhotoLibrary/Cells/CellTwo.swift
@@ -0,0 +1,13 @@
+//
+//  CellTwo.swift
+//  PhotoLibrary
+//
+//  Created by Pavlov Matthew on 12.04.2021.
+//
+
+import UIKit
+
+class CellTwo: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    
+}
